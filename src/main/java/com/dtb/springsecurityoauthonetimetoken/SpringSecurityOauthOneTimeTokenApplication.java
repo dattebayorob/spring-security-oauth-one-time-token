73199de3618dd738cd2c0f601b8008827abd5145
dattebayoRob <robson.william65@gmail.com>
2020-04-22 21:14:55 -0300

    Initial commit

---
+++ src/main/java/com/dtb/springsecurityoauthonetimetoken/SpringSecurityOauthOneTimeTokenApplication.java
@@ -0,0 +1,13 @@
+package com.dtb.springsecurityoauthonetimetoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauthOneTimeTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOauthOneTimeTokenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dtb/springsecurityoauthonetimetoken/SpringSecurityOauthOneTimeTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtb.springsecurityoauthonetimetoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauthOneTimeTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
